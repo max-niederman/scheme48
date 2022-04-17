09e5600c01a6ecf207ff89d6b5a13db357f4b9d8
Max Niederman <max@maxniederman.com>
2022-04-16 22:30:13 -0700

    build: bootstrap with Nix and Cabal

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (pkgs) lib haskell haskellPackages;
+
+          name = "vanessa";
+
+          project = devTools:
+            haskellPackages.developPackage {
+              root = lib.sourceFilesBySuffices ./. [ ".cabal" ".hs" ];
+              inherit name;
+              returnShellEnv = devTools != [ ];
+
+              modifier = with haskell.lib.compose;
+                (lib.trivial.flip lib.trivial.pipe) [
+                  (addBuildTools devTools)
+                  dontHaddock
+                  enableStaticLibraries
+                  justStaticExecutables
+                  disableLibraryProfiling
+                  disableExecutableProfiling
+                ];
+            };
+        in
+        rec {
+          packages.vanessa = project [ ];
+          defaultPackage = packages.vanessa;
+
+          devShell = project (with haskellPackages; [
+            # cabal
+            cabal-fmt
+            cabal-install
+          ]);
+        }
+      );
+}
