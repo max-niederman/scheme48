d44d18b318bcbccbd7db41d44576cf11ef180725
Max Niederman <max@maxniederman.com>
2022-04-17 20:26:57 -0700

    feat(parser): implement basic parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,91 @@
 module Main where
 
+import Numeric (readHex, readOct)
+import System.Environment (getArgs)
+import Text.ParserCombinators.Parsec
+
+whitespace :: Parser ()
+whitespace = skipMany1 space
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+  deriving (Show)
+
+parseExpr :: Parser LispVal
+parseExpr = choice [parseQuoted, parseList, parseString, parseNumber, parseAtom]
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  expr <- parseExpr
+  return $ List [Atom "quote", expr]
+
+parseList :: Parser LispVal
+parseList = do
+  char '('
+  proper <- sepEndBy parseExpr whitespace
+  list <- DottedList proper <$> (char '.' >> spaces >> parseExpr) <|> return (List proper)
+  char ')'
+  return list
+
+--   TODO: implement the rest of the R5RS number spec i.e. floating points, binary notation, complex numbers
+parseNumber :: Parser LispVal
+parseNumber =
+  try parseEscaped <|> parseDecimal
+  where
+    parseDecimal :: Parser LispVal
+    parseDecimal = Number . read <$> many1 digit
+
+    parseEscaped :: Parser LispVal
+    parseEscaped =
+      char '#'
+        >> choice
+          [ char 'o' >> Number . fst . head . readOct <$> many1 octDigit,
+            char 'd' >> parseDecimal,
+            char 'x' >> Number . fst . head . readHex <$> many1 hexDigit
+          ]
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  contents <- many (parseEscape <|> noneOf ['"'])
+  char '"'
+  return $ String contents
+  where
+    parseEscape :: Parser Char
+    parseEscape = do
+      char '\\'
+      code <- oneOf ['\\', '"', 'n', 'r', 't']
+      return $ case code of
+        'n' -> '\n'
+        'r' -> '\r'
+        't' -> '\t'
+        _ -> code
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+  Left err -> "Error:\n" ++ show err
+  Right val -> "Parsed:\n" ++ show val
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
